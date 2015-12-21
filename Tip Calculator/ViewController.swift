ba7d8ba07eaf8d34be6b43e8073f106dcd6deead
Krzysztof Sendor <sendor.krzysztof@gmail.com>
2015-12-21 22:20:55 +0100

    Created first model and view controller connected with storyboard

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -1,18 +1,45 @@
-//
-//  ViewController.swift
-//  Tip Calculator
-//
-//  Created by Krzysztof Sendor on 21.12.2015.
-//  Copyright © 2015 Krzysztof Sendor. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
 
+    @IBOutlet var totalTextField : UITextField!
+    @IBOutlet var taxPctSlider : UISlider!
+    @IBOutlet var taxPctLabel : UILabel!
+    @IBOutlet var resultsTextView : UITextView!
+    
+    @IBAction func calculateTapped(sender : AnyObject) {
+        tipCalc.total = Double((totalTextField.text! as NSString).doubleValue)
+        
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        
+        for (tipPct, tipValue) in possibleTips {
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        
+        resultsTextView.text = results
+    }
+    @IBAction func taxPercentageChanged(sender : AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    @IBAction func viewTapped(sender : AnyObject) {
+        totalTextField.resignFirstResponder()
+    }
+    
+    func refreshUI(){
+        totalTextField.text = String(format: "%0.2f", tipCalc.total)
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        resultsTextView.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
