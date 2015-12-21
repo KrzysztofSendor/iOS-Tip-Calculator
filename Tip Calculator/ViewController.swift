6680a81f0bdaeb05b030927ceec85051857b24da
Krzysztof Sendor <sendor.krzysztof@gmail.com>
2015-12-21 20:19:49 +0100

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Krzysztof Sendor on 21.12.2015.
+//  Copyright © 2015 Krzysztof Sendor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
